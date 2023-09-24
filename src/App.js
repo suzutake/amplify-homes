ce23697c3d69ee6c3fd90c43dc65dca5bb02a069
suzutake <suzukitakeru@gmail.com>
2023-09-25 04:14:31 +0900

    cannot show amplify, but no error ver

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  ItemCardCollection 
+ } from './ui-components';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <ItemCardCollection />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
